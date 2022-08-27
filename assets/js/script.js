72325e54c0fad00d2e001c0b7e3135207d3ad409
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-27 13:04:19 -0700

    added get API function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 // defining variables
 
 // functions 
+
+// todo add fetch call for weather api 
+function getAPI(){
+    var requestURL = "";
+    // API KEY : f18de7fe10f46ccca13adc41b61d567d
+
+    fetch(requestURL)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
+}
