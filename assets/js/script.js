f858a9ead1ec2c6041d8a4cf696468ae6de6a20a
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-27 17:51:29 -0700

    added local storage and a get from local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 // defining variables
+var searchBtnEl = $("#searchBtn");
+var searchCityEl = $("#searchCity");
+var recentCitiesList = $(".list-group");
+
 
 // functions 
+// get city, add it to local storage
+// todo get its long and lat
+// todo make local storage array
+function getCity(event){
+    var city = event.currentTarget.previousElementSibling.value;
+    console.log(city);
+    localStorage.setItem("city", city);
+
+    makeRecentCities();
+}
+
+function makeRecentCities(){
+    var savedCities = localStorage.getItem("city");
+    var savedCity1 = $("<li>").text(savedCities).addClass("list-group-item")
+    recentCitiesList.append(savedCity1);
+}
 
 // todo add fetch call for weather api 
 function getAPI(){
@@ -16,16 +36,18 @@ function getAPI(){
         })
 }
 
-getAPI();
+searchBtnEl.on("click", getCity);
+makeRecentCities();
 
-// when search city, add to local storage, get the calls for the apis 
 
+// todo when search city, add to local storage, get the calls for the apis 
+
+// todo pass city, get longitude
 // geodirect - https://openweathermap.org/api/geocoding-api
-// pass city, get longitude
 
+// todo five days weather forcast creating card for five day, pushing info onto card
 // one call api - https://openweathermap.org/api/one-cal
-// five days weather forcast 
-// creating card for five day, pushing info onto card 
 
-//better weather icons 
+
+// todo better weather icons 
 //https://erikflowers.github.io/weather-icons/
