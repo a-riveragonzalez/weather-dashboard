6d5d8a4356afa5425aafe902602a19a5e8805134
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-29 01:57:08 -0700

    updated forcast blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ var searchBtnEl = $("#searchBtn");
 var searchCityEl = $("#searchCity");
 var currentWeatherEl = $(".current-weather");
 var fiveDayForcastEl = $(".five-day-forcast");
-var cardBodyEl = $(".card-body");
 var recentCitiesList = $(".list-group");
 
 var today = moment().format("MMMM Do")
@@ -79,6 +78,8 @@ function getAPI(city){
                     var forcastTemp = "Temp: " + data2.list[indexNumber].main.temp + "°F";
                     var forcastWind = "Wind: " + data2.list[indexNumber].wind.speed + " MPH";
                     var forcastHumidity = "Humidity: " + data2.list[indexNumber].main.humidity + "%";
+
+                    var cardBodyEl = $("<div>").addClass("card-body my-2");
                     
                     var dateCard = $("<h5>").text(forcastDate).addClass("card-title");
                     var iconCard = $("<p>").text(forcastIcon).addClass("card-text");
@@ -86,7 +87,10 @@ function getAPI(city){
                     var windCard = $("<p>").text(forcastWind).addClass("card-text");
                     var humidityCard = $("<p>").text(forcastHumidity).addClass("card-text");
 
+                    fiveDayForcastEl.addClass("card text-white bg-secondary mb-3");
+                    fiveDayForcastEl.append(cardBodyEl);
                     cardBodyEl.append(dateCard , iconCard , tempCard , windCard , humidityCard);
+
                 })
 
 
