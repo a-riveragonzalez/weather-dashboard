2d3a39c07e5b948d90544d76cbeb16393435e016
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-30 15:28:48 -0700

    fixed the weather one call

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,7 @@ function getAPI(city){
             var lat = data[0].lat;
             var lon = data[0].lon;
 
-            var url = "https://api.openweathermap.org/data/2.5/forecast?appid=f18de7fe10f46ccca13adc41b61d567d&units=imperial&lat=" + lat + "&lon=" + lon;
+            var url = "https://api.openweathermap.org/data/2.5/onecall?appid=9b35244b1b7b8578e6c231fd7654c186&units=imperial&lat=" + lat + "&lon=" + lon;
 
             fetch(url)
             .then(function(response2){
@@ -50,34 +50,36 @@ function getAPI(city){
                 }
             }).then(function(data2) {
                 console.log(data2);
-                var cityName = data2.city.name;
-                var todayIcon = data2.list[0].weather[0].icon;
-                var todayTemp = "Temp: " + data2.list[0].main.temp + "°F";
-                var todayWind = "Wind: " + data2.list[0].wind.speed + " MPH";
-                var todayHumidity = "Humidity: " + data2.list[0].main.humidity + "%";
-                var todayUVIndex;
+
+                // var cityName = data2.city.name;
+                var todayIcon = data2.current.weather[0].icon;
+                var todayTemp = "Temp: " + data2.current.temp + "°F";
+                var todayWind = "Wind: " + data2.current.wind_speed + " MPH";
+                var todayHumidity = "Humidity: " + data2.current.humidity + "%";
+                var todayUVIndex = data2.current.uvi;
 
                 // generate current weather card
                 // todo make H3 with name of city, date, and icon of current weather
-                var cityHeading = $("<h2>").text(cityName + " : " + today).addClass("text-dark border-bottom border-dark");
+                // var cityHeading = $("<h2>").text(cityName + " : " + today).addClass("text-dark border-bottom border-dark");
                 // todo make p's with temp, wind, humidity, and uv index
                 var temp = $("<p>").text(todayTemp).addClass("text-dark mb-0");
                 var wind = $("<p>").text(todayWind).addClass("text-dark mb-0");
                 var humidity = $("<p>").text(todayHumidity).addClass("text-dark mb-0");
 
-                currentWeatherEl.append(cityHeading , temp , wind , humidity);
+                currentWeatherEl.append(temp , wind , humidity); //cityHeading
 
                 // generate five day forcast cards
                 // todo make card with date, icon, temp, wind, and humidity
                 // these are the index numbers for next five days
-                var indexNumbers = ["9", "17", "25", "33", "39"]; 
+                var indexNumbers = ["1", "2", "3", "4", "5"]; 
 
                 indexNumbers.forEach(function(indexNumber){
-                    var forcastDate = moment(data2.list[indexNumber].dt,"X").format("M/D/YYYY");
-                    var forcastIcon = data2.list[indexNumber].weather[0].icon;
-                    var forcastTemp = "Temp: " + data2.list[indexNumber].main.temp + "°F";
-                    var forcastWind = "Wind: " + data2.list[indexNumber].wind.speed + " MPH";
-                    var forcastHumidity = "Humidity: " + data2.list[indexNumber].main.humidity + "%";
+                    var forcastDate = moment(data2.daily[indexNumber].dt,"X").format("M/D/YYYY");
+                    var forcastIcon = data2.daily[indexNumber].weather[0].icon;
+                    console.log(forcastDate);
+                    var forcastTemp = "Temp: " + data2.daily[indexNumber].temp.day + "°F";
+                    var forcastWind = "Wind: " + data2.daily[indexNumber].wind_speed + " MPH";
+                    var forcastHumidity = "Humidity: " + data2.daily[indexNumber].humidity + "%";
 
                     var cardBodyEl = $("<div>").addClass("card-body my-2");
                     
