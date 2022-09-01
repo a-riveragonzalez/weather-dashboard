64314930e776f18033213d6191bdff1e4323e211
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-31 22:52:23 -0700

    added weather icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,6 +65,7 @@ function makeRecentCities(){
 // todo make function for : if i click on recent cities, then getAPI(city) 
 // todo add event listener to recentCitiesList
 
+
 // todo fetch current weather, weather forcast 5 days 
 function getAPI(city){
     var city = city;
@@ -88,38 +89,36 @@ function getAPI(city){
                     return response2.json();
                 }
                 else{
-                    // todo make 404 error page populate
                     console.log("there's an error!")
                 }
             }).then(function(data2) {
                 console.log(data2);
 
                 var cityName = city.toUpperCase();
-                var todayIcon = data2.current.weather[0].icon;
+                var todayIcon = "http://openweathermap.org/img/wn/" + data2.current.weather[0].icon + "@2x.png";
                 var todayTemp = "Temp: " + data2.current.temp + "°F";
                 var todayWind = "Wind: " + data2.current.wind_speed + " MPH";
                 var todayHumidity = "Humidity: " + data2.current.humidity + "%";
-                var todayUVIndex = "UV Index: " + data2.current.uvi;
+                var todayUVIndex = data2.current.uvi;
 
                 // generate current weather card
-                // todo make H3 with name of city, date, and icon of current weather
-                var cityHeading = $("<h2>").text(cityName + " : " + today + " " + todayIcon).addClass("text-dark border-bottom border-dark");
-                // todo make p's with temp, wind, humidity, and uv index
+                var cityHeading = $("<h2>").text(cityName + " : " + today).addClass("text-dark border-bottom border-dark");
+                var iconPic = $("<img>").attr("src", todayIcon);
+                // todo make uv index change class depending on its value
                 var temp = $("<p>").text(todayTemp).addClass("text-dark mb-0");
                 var wind = $("<p>").text(todayWind).addClass("text-dark mb-0");
                 var humidity = $("<p>").text(todayHumidity).addClass("text-dark mb-0");
-                var uvIndex = $("<p>").text(todayUVIndex).addClass("text-dark mb-0");
+                var uvIndex = $("<p>").text("UV Index: " + todayUVIndex).addClass("text-dark mb-0");
 
-                currentWeatherEl.append(cityHeading, temp , wind , humidity, uvIndex); 
+                currentWeatherEl.append(cityHeading, iconPic, temp , wind , humidity, uvIndex); 
 
                 // generate five day forcast cards
-                // todo make card with date, icon, temp, wind, and humidity
                 // these are the index numbers for next five days
                 var indexNumbers = ["1", "2", "3", "4", "5"]; 
 
                 indexNumbers.forEach(function(indexNumber){
                     var forcastDate = moment(data2.daily[indexNumber].dt,"X").format("M/D/YYYY");
-                    var forcastIcon = data2.daily[indexNumber].weather[0].icon;
+                    var forcastIcon = "http://openweathermap.org/img/wn/" + data2.daily[indexNumber].weather[0].icon + "@2x.png";
                     var forcastTemp = "Temp: " + data2.daily[indexNumber].temp.day + "°F";
                     var forcastWind = "Wind: " + data2.daily[indexNumber].wind_speed + " MPH";
                     var forcastHumidity = "Humidity: " + data2.daily[indexNumber].humidity + "%";
@@ -127,12 +126,12 @@ function getAPI(city){
                     var cardBodyEl = $("<div>").addClass("card-body my-2");
                     
                     var dateCard = $("<h5>").text(forcastDate).addClass("card-title");
-                    var iconCard = $("<p>").text(forcastIcon).addClass("card-text");
+                    var iconCard = $("<img>").attr("src", forcastIcon).addClass("card-text");
                     var tempCard = $("<p>").text(forcastTemp).addClass("card-text");
                     var windCard = $("<p>").text(forcastWind).addClass("card-text");
                     var humidityCard = $("<p>").text(forcastHumidity).addClass("card-text");
 
-                    fiveDayForcastEl.addClass("card text-white bg-secondary mb-3");
+                    fiveDayForcastEl.addClass("card-deck text-white bg-secondary mb-3");
                     fiveDayForcastEl.append(cardBodyEl);
                     cardBodyEl.append(dateCard , iconCard , tempCard , windCard , humidityCard);
 
